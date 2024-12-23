870775305b3648283bdfbf2b98e3fa39b1c91888
svyat-r <madeinukrainego@gmail.com>
2024-12-23 19:18:50 +0100

    fix: fixed validators names in the ServiceProvider

--- src/ServiceProvider.php
+++ src/ServiceProvider.php
@@ -4,10 +4,10 @@ declare(strict_types=1);
 
 namespace Coodde\LaravelMailChecker;
 
-use Coodde\LaravelMailChecker\Rules\MailDomainsValidation;
-use Coodde\LaravelMailChecker\Rules\MailListedValidation;
-use Coodde\LaravelMailChecker\Rules\MailRegionsValidation;
-use Coodde\LaravelMailChecker\Rules\MailValidation;
+use Coodde\LaravelMailChecker\Rules\MailDomainsValidator;
+use Coodde\LaravelMailChecker\Rules\MailListedValidator;
+use Coodde\LaravelMailChecker\Rules\MailRegionsValidator;
+use Coodde\LaravelMailChecker\Rules\MailValidator;
 use Illuminate\Support\ServiceProvider as SupportServiceProvider;
 
 class ServiceProvider extends SupportServiceProvider
@@ -23,10 +23,10 @@ class ServiceProvider extends SupportServiceProvider
     public function boot(): void
     {
         // Register custom validation rules
-        Validator::extend('mail_check', MailValidation::class);
-        Validator::extend('mail_check_domains', MailDomainsValidation::class);
-        Validator::extend('mail_check_listed', MailListedValidation::class);
-        Validator::extend('mail_check_regions', MailRegionsValidation::class);
+        Validator::extend('mail_check', MailValidator::class);
+        Validator::extend('mail_check_domains', MailDomainsValidator::class);
+        Validator::extend('mail_check_listed', MailListedValidator::class);
+        Validator::extend('mail_check_regions', MailRegionsValidator::class);
 
         // Publish package's configuration file to the application's configuration directory
         $this->publishes([
